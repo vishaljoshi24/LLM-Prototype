6ec85c815e2024e03a80af54adf2acae84cc5fde
Jack Burnett <jackjburnett@gmail.com>
2024-01-05 13:42:43 +0000

    Initial commit

---
+++ PersonaList.py
@@ -0,0 +1,3 @@
+import pandas as pd
+personas_dataframe=pd.read_csv('files/personas.csv')
+personas=personas_dataframe["Persona"].tolist()
---
+++ streamlit_app.py
@@ -0,0 +1,89 @@
+import streamlit as st
+import replicate
+import os
+import random
+import PersonaList
+
+# App title
+st.set_page_config(page_title="Persona Chatbot")
+
+# Persona Gen
+if "rn" not in st.session_state:
+    st.session_state["rn"] = random.randint(0, len(PersonaList.personas))
+
+if "persona" not in st.session_state:
+    st.session_state["persona"]=PersonaList.personas[st.session_state["rn"]]
+
+# Replicate Credentials
+with st.sidebar:
+    st.title('Persona Chatbot')
+    replicate_api = st.secrets['REPLICATE_API_TOKEN']
+
+
+    # Refactored from https://github.com/a16z-infra/llama2-chatbot
+    st.subheader('Models and parameters')
+    selected_model = st.sidebar.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B', 'Llama2-70B'], key='selected_model')
+    if selected_model == 'Llama2-7B':
+        llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
+    elif selected_model == 'Llama2-13B':
+        llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
+    else:
+        llm = 'replicate/llama70b-v2-chat:e951f18578850b652510200860fc4ea62b3b16fac280f83ff32282f87bbd2e48'
+    
+    temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
+    top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
+    max_length = st.sidebar.slider('max_length', min_value=64, max_value=4096, value=512, step=8)
+    
+os.environ['REPLICATE_API_TOKEN'] = replicate_api
+
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "Hello! How are you?"}]
+
+# Display or clear chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+def clear_chat_history():
+    st.session_state.messages = [{"role": "assistant", "content": "Hello! How are you?"}]
+st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+
+
+
+# Function for generating LLaMA2 response
+def generate_llama2_response(prompt_input):
+    string_dialogue = f"""<<SYS>>
+You are undertaking the persona of: {st.session_state['persona']}. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'.
+<</SYS>>
+Current conversation:
+"""
+    for dict_message in st.session_state.messages:
+        if dict_message["role"] == "user":
+            string_dialogue += "User: " + dict_message["content"] + "\n\n"
+        else:
+            string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
+    output = replicate.run(llm, 
+                           input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
+                                  "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1})
+    return output
+
+# User-provided prompt
+if prompt := st.chat_input(disabled=not replicate_api):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = generate_llama2_response(prompt)
+            placeholder = st.empty()
+            full_response = ''
+            for item in response:
+                full_response += item
+                placeholder.markdown(full_response)
+            placeholder.markdown(full_response)
+    message = {"role": "assistant", "content": full_response}
+    st.session_state.messages.append(message)
