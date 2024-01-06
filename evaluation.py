7954876da8e275a266b6b816a26519d23331ef05
Jack Burnett <jackjburnett@gmail.com>
2024-01-06 15:45:46 +0000

    Updated csv generation

--- evaluation.py
+++ evaluation.py
@@ -1,8 +1,15 @@
 import csv
+import os
 
 
 def submit_rating(selected_model, temperature, top_p, top_k, repetition, max_length, persona, informativeness,
                   coherency, fluency):
+    if not os.path.isfile("./evaluation.csv"):
+        fields = ["model", "temperature", "top_p", "top_k", "repetition", "max_length", "persona", "informativeness",
+                  "coherency", "fluency"]
+        with open(r'evaluation.csv', 'a', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(fields)
     if informativeness:
         informativeness = 10
     else:
