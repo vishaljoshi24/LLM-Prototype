cd037d4d01a36f1d7e0483080477733e5ed56259
Jack Burnett <jackjburnett@gmail.com>
2024-03-05 23:57:08 +0000

    Deletes Personas and Renamed LLM

--- PersonaList.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import random
-
-personas_dataframe = pd.read_csv('files/personas.csv')
-personas = personas_dataframe["Persona"].tolist()
-
-
-def random_persona():
-    return personas[random.randint(0, len(personas))]
