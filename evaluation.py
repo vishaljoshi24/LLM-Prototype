0656f720a1f119d40ad5704e69b0888782b46c32
Jack Burnett <jackjburnett@gmail.com>
2024-03-05 23:53:02 +0000

    Update evaluation.py
    Now takes prompt argument rather than personas

--- evaluation.py
+++ evaluation.py
@@ -2,20 +2,14 @@ import csv
 import os
 
 
-def submit_rating(selected_model, temperature, top_p, top_k, repetition, max_length, persona, informativeness,
-                  coherency, fluency):
+# Submits ratings for conversations, outputs model parameters and prompts
+def submit_rating(selected_model, prompt, temperature, top_p, top_k, repetition, max_length, coherency, fluency):
     if not os.path.isfile("./evaluation.csv"):
-        fields = ["model", "temperature", "top_p", "top_k", "repetition", "max_length", "persona", "informativeness",
-                  "coherency", "fluency"]
+        fields = ["model", " prompt", "temperature", "top_p", "top_k", "repetition", "max_length", "coherency", "fluency"]
         with open(r'evaluation.csv', 'a', newline='') as f:
             writer = csv.writer(f)
             writer.writerow(fields)
-    if informativeness:
-        informativeness = 10
-    else:
-        informativeness = 0
-    fields = [selected_model, temperature, top_p, top_k, repetition, max_length, persona, informativeness,
-              coherency, fluency]
+    fields = [selected_model, prompt, temperature, top_p, top_k, repetition, max_length, coherency, fluency]
     with open(r'evaluation.csv', 'a', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(fields)
