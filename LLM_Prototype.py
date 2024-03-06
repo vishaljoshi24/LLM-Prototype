09985d7a255f3584e5ab24d57c1df734402cbf9d
Jack Burnett <jackjburnett@gmail.com>
2024-03-06 00:02:32 +0000

    Refactored Interface

--- LLM_Prototype.py
+++ LLM_Prototype.py
@@ -1,26 +1,10 @@
 import streamlit as st
 import random
-import PersonaList
 import llama2local
 import evaluation
 
 # App title
-st.set_page_config(page_title="Persona Chatbot")
-
-# Persona Gen
-if "rn" not in st.session_state:
-    st.session_state["rn"] = random.randint(0, len(PersonaList.personas))
-
-if "persona" not in st.session_state:
-    st.session_state["persona"] = [(PersonaList.personas[st.session_state["rn"]]).replace('.', ',')]
-    st.session_state["persona"].append(PersonaList.random_persona().replace('.', ','))
-    st.session_state["persona"].append(PersonaList.random_persona().replace('.', ','))
-    st.session_state["persona"].append(PersonaList.random_persona().replace('.', ','))
-
-if "order" not in st.session_state:
-    order = list(range(4))
-    random.shuffle(order)
-    st.session_state["order"] = order
+st.set_page_config(page_title="DnD LLM Prototype")
 
 if "show_advanced" not in st.session_state:
     st.session_state["show_advanced"] = False
@@ -32,21 +16,18 @@ def advanced_change():
 
 # Replicate Credentials
 with st.sidebar:
-    st.title('Persona Chatbot')
+    st.title('DnD LLM Prototype')
 
-    # Refactored from https://github.com/a16z-infra/llama2-chatbot
-    st.subheader('Models and parameters')
-    selected_model = st.sidebar.selectbox('Choose a Large Language Model',
-                                          ['LLaMa2-7B-Chat', 'LLaMa2-13B-Chat', 'GPT-3.5-turbo-1106'],
-                                          key='selected_model')
-
-    temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.72, step=0.01,
-                                    disabled=(selected_model == "GPT-3.5-turbo-1106"))
-    top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.73, step=0.01,
-                              disabled=(selected_model == "GPT-3.5-turbo-1106"))
-    st.sidebar.button('Toggle Advanced Options', on_click=advanced_change)
-
+    st.sidebar.button('Developer Settings', on_click=advanced_change)
     if st.session_state["show_advanced"]:
+        st.subheader('Parameters')
+        selected_model = st.sidebar.selectbox('Choose a Large Language Model',
+                                              ['LLaMa2-7B-Chat', 'LLaMa2-13B-Chat', 'GPT-3.5-turbo-1106'],
+                                              key='selected_model')
+        temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.72, step=0.01,
+                                        disabled=(selected_model == "GPT-3.5-turbo-1106"))
+        top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.73, step=0.01,
+                                  disabled=(selected_model == "GPT-3.5-turbo-1106"))
         top_k = st.sidebar.slider('top_k', min_value=0, max_value=100, value=0, step=1,
                                   disabled=(selected_model == "GPT-3.5-turbo-1106"))
         repetition = st.sidebar.slider('repetition_penalty', min_value=0.0, max_value=2.0, value=1.1, step=0.01,
@@ -54,27 +35,20 @@ with st.sidebar:
         max_length = st.sidebar.slider('max_length', min_value=64, max_value=4096, value=512, step=8,
                                        disabled=(selected_model == "GPT-3.5-turbo-1106"))
     else:
+        selected_model='LLaMa2-13B-Chat'
+        temperature=0.72
+        top_p=0.73
         top_k = 0
         repetition = 1.1
         max_length = 512
 
-    st.markdown('----')
-    st.write("**Rate the Persona**")
-    perceived_persona = st.radio("Which persona do you feel is being represented:",
-                                 [st.session_state["persona"][st.session_state["order"][0]],
-                                  st.session_state["persona"][st.session_state["order"][1]],
-                                  st.session_state["persona"][st.session_state["order"][2]],
-                                  st.session_state["persona"][st.session_state["order"][3]]])
-
+    st.write("**Rate the Responses**")
     coherency = st.slider('Coherency (how well does the AI match the persona)', min_value=0, max_value=10, value=5,
                           step=1)
     fluency = st.slider('Fluency (how natural is the conversation)', min_value=0, max_value=10, value=5, step=1)
-    st.sidebar.button(label='Rate Persona',
+    st.sidebar.button(label='Rate Conversation',
                       on_click=evaluation.submit_rating,
-                      args=(selected_model, temperature, top_p, top_k, repetition, max_length,
-                            st.session_state["persona"][0],
-                            st.session_state["persona"][0] == perceived_persona,
-                            coherency, fluency))
+                      args=(selected_model, temperature, top_p, top_k, repetition, max_length, coherency, fluency))
     st.markdown('----')
 
 
--- evaluation.py
+++ evaluation.py
@@ -3,12 +3,16 @@ import os
 
 
 # Submits ratings for conversations, outputs model parameters and prompts
-def submit_rating(selected_model, prompt, temperature, top_p, top_k, repetition, max_length, coherency, fluency):
+def submit_rating(selected_model, temperature, top_p, top_k, repetition, max_length, coherency, fluency):
+    # Create the CSV if it doesn't exist
     if not os.path.isfile("./evaluation.csv"):
         fields = ["model", " prompt", "temperature", "top_p", "top_k", "repetition", "max_length", "coherency", "fluency"]
         with open(r'evaluation.csv', 'a', newline='') as f:
             writer = csv.writer(f)
             writer.writerow(fields)
+
+    prompt=""
+
     fields = [selected_model, prompt, temperature, top_p, top_k, repetition, max_length, coherency, fluency]
     with open(r'evaluation.csv', 'a', newline='') as f:
         writer = csv.writer(f)
