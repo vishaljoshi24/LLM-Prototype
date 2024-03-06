b9f0e21dbce72fecd0993d6a7c3f5f79c7b718c7
Jack Burnett <jackjburnett@gmail.com>
2024-03-06 00:03:17 +0000

    Added comments to evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -11,8 +11,10 @@ def submit_rating(selected_model, temperature, top_p, top_k, repetition, max_len
             writer = csv.writer(f)
             writer.writerow(fields)
 
+    # Needs to be updated with prompt collection method
     prompt=""
 
+    # Gets all fields and stores them in evaluation.csv
     fields = [selected_model, prompt, temperature, top_p, top_k, repetition, max_length, coherency, fluency]
     with open(r'evaluation.csv', 'a', newline='') as f:
         writer = csv.writer(f)
