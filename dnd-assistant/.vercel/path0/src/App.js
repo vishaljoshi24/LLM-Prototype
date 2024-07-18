b45573f64e5b2f6063c016bbf468065f657a8ca3
Vishal Joshi <we19383@bristol.ac.uk>
2024-07-18 15:30:06 +0100

    App deployment

---
+++ dnd-assistant/.vercel/path0/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Chat from './components/Chat';
+import './components/Chat.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
